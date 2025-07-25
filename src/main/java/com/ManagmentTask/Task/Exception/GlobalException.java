dd4e483145bbc3541993ef26dfe032a8874bbdca
ayushchaudhary21 <chaudharyayush269@gmail.com>
2025-07-26 04:37:57 +0530

    All the exception done

---
+++ src/main/java/com/ManagmentTask/Task/Exception/GlobalException.java
@@ -0,0 +1,45 @@
+package com.ManagmentTask.Task.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(EmployeeNotFound.class)
+    public ResponseEntity<ErrorResponse> handleEmployeeNotFound(EmployeeNotFound e)
+    {
+           ErrorResponse error= new ErrorResponse(
+                   HttpStatus.NOT_FOUND.value(),
+                   e.getMessage(),
+                   LocalDateTime.now()
+           );
+           return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(TaskNotFound.class)
+    public ResponseEntity<ErrorResponse> handleTaskNotFound(TaskNotFound ex)
+    {
+          ErrorResponse error=new ErrorResponse(
+                  HttpStatus.NOT_FOUND.value(),
+                  ex.getMessage(),
+                  LocalDateTime.now()
+          );
+          return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InternalException.class)
+    public ResponseEntity<ErrorResponse>handleInternalException(InternalException ex)
+    {
+         ErrorResponse error=new ErrorResponse(
+                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                 ex.getMessage(),
+                 LocalDateTime.now()
+         );
+         return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
